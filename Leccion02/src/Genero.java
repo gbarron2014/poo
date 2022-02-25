8ded191b8bac883edaa2fd0cc41811723a087468
Gabriel Barron Rodríguez <gbarron@utng.edu.mx>
2022-02-25 05:46:58 -0600

    Versión Inicial

---
+++ Leccion02/src/Genero.java
@@ -0,0 +1,21 @@
+/**
+ * Descripcion: Clase que representa el Genero de una persona
+ * Autor: Gabriel Barron
+ * Fecha: 24 Febrero 2022 
+*/
+public class Genero {
+	public String nombre;
+
+	Genero() {
+		System.out.println("Construyendo un objeto Genero");
+	}
+
+	Genero (String pNombre) {
+		this.nombre = pNombre;
+		System.out.println("Construyendo un objeto Genero " + this.nombre);
+	}
+
+	public String toString() {
+		return "Nombre: " + this.nombre;
+	}
+}
---
+++ Leccion02/src/TestGenero.java
@@ -0,0 +1,7 @@
+public class TestGenero {
+	public static void main(String[] args) {
+		
+		Genero male = new Genero("Masculino");
+		Genero female = new Genero("Femenino");
+	}
+}
